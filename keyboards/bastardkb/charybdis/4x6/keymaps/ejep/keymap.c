068076fd62cb40a6bee1b4be43f3dde605916f75
Emily Price <emily.j.price.nth@gmail.com>
2024-10-09 18:38:10 +0100

    Add grave

--- keyboards/bastardkb/charybdis/4x6/keymaps/ejep/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/ejep/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, EE_CLR,     KC_PPLS,   KC_P4,   KC_P5,   KC_P6, KC_BSLS, KC_EQL,
+       RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, EE_CLR,     KC_PPLS,   KC_P4,   KC_P5,   KC_GRAVE, KC_BSLS, KC_EQL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
       RGB_RMOD, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, QK_BOOT,   KC_DELETE,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
